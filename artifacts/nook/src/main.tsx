6f65c53d3c3fb7a3ebc9093411dea469aac77981
Tanmay Tripathi <TanmayTripathi7525@gmail.com>
2026-06-13 22:58:03 +0530

    Fix Railway API URL

--- artifacts/nook/src/main.tsx
+++ artifacts/nook/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { setBaseUrl } from "../../lib/api-client-react/src/custom-fetch";
+import { setBaseUrl } from "../../../lib/api-client-react/src/custom-fetch";
 
 setBaseUrl("https://workspaceapi-server-production-e7e1.up.railway.app");
 
