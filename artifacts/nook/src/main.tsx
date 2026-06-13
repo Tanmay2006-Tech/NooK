51f742e7fd7f641d8216ee76996aaf72985a4cd6
Tanmay Tripathi <TanmayTripathi7525@gmail.com>
2026-06-13 22:56:25 +0530

    Connect frontend to Railway API

--- artifacts/nook/src/main.tsx
+++ artifacts/nook/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "../../lib/api-client-react/src/custom-fetch";
+
+setBaseUrl("https://workspaceapi-server-production-e7e1.up.railway.app");
+
 createRoot(document.getElementById("root")!).render(<App />);
